eccbbe7d4d8dca73ea5d711b3312f6da1d472803
cris <crisrelayo17@gmail.com>
2026-07-01 13:05:47 +0800

    Add simple css design

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let term1, operator, term2;
 
-function operate(operator, term1, term2){
-  switch(operator){
+function operate(operator, term1, term2) {
+  switch(operator) {
     case '+':
       add(term1, term2);
     case '-':
