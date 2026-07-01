648e15b6de916834f096745a4b11ab5ac37bf3ad
cris <crisrelayo17@gmail.com>
2026-07-01 18:27:52 +0800

    Fix multiple bugs
    1: press equal without any values
    2: display message when divide by 0
    3: press operator without any values

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const opBtns = Array.from(
 const displayResult = document.querySelector('.calcu-result');
 let ans = 0;
 const equalBtn = document.querySelector('.calcu-equal');
+const acBtn = document.querySelector('.calcu-all-clear')
 
 for(const btn of opBtns) {
   if(btn.textContent === 'AC') {
@@ -38,9 +39,17 @@ for(const btn of opBtns) {
     continue;
   }else if(btn.textContent === '=') {
     btn.addEventListener('click', () => {
+      if(operator === '') return;
       if((typeof term2) === 'string') term2 = +(term2);
+
       ans = operate(operator, term1, term2);
+      if(ans === 'divBy0'){
+        acBtn.click();
+        displayResult.textContent = 'Nice try loser';
+        return;
+      }
       if(!(Number.isInteger(ans))) ans = ans.toFixed(2);
+
       console.log(`ans: ${ans}`); // console.log
       displayResult.textContent = ans;
     });
@@ -48,9 +57,11 @@ for(const btn of opBtns) {
   }
 
   btn.addEventListener('click', () => {
+    if(term1 === '') return;
     if(term2 !== '') {
       term2 = +(term2);
       equalBtn.click();
+      if(ans === 0) return;
       displayExpr.textContent = ans;
       term1 = ans;
       term2 = '';
@@ -66,32 +77,21 @@ for(const btn of opBtns) {
 function operate(operator, term1, term2) {
   switch(operator) {
     case '+':
-      return add(term1, term2);
+      return term1 + term2;;
       break;
     case '-':
-      return subtract(term1, term2);
+      return term1 - term2;
       break;
     case '×':
-      return multiply(term1, term2);
+      return term1 * term2;
       break;
     case '÷':
-      return divide(term1, term2);
+      if(term2 === 0) return 'divBy0';
+      return term1 / term2;
+      break;
+    default:
+      console.log('no operator, returning 0');
+      return 0;
       break;
   }
-}
-
-function add(a, b) {
-	return a + b;
-};
-
-function subtract(a, b) {
-	return a - b;
-};
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
 }
